d56da4e548488c09ab3f6ed63d76664a1354baa4
viktormashyka <99292051+viktormashyka@users.noreply.github.com>
2024-04-24 22:09:17 +0100

    goit-node-cli

---
+++ index.js
@@ -0,0 +1,47 @@
+import { program } from "commander";
+import {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContactById,
+} from "./src/contacts.js";
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await listContacts();
+      return console.table(allContacts);
+
+    case "get":
+      const oneContact = await getContactById(id);
+      return console.log(oneContact);
+
+    case "add":
+      const newContact = await addContact(name, email, phone);
+      return console.log(newContact);
+
+    case "remove":
+      const removedContact = await removeContact(id);
+      return console.log(removedContact);
+
+    case "update":
+      const updatedContact = await updateContactById(id);
+      return console.log(updatedContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
---
+++ src/contacts.js
@@ -0,0 +1,59 @@
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
+
+const contactsPath = path.resolve("src/db", "contacts.json");
+
+const updateContacts = (contacts) =>
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+export async function listContacts() {
+  // ...твій код. Повертає масив контактів.
+  const data = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(data);
+}
+
+export async function getContactById(contactId) {
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === contactId) || null;
+  return result;
+}
+
+export async function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await updateContacts(contacts);
+  return result;
+}
+
+export async function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту (з id).
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  const contacts = await listContacts();
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+
+export async function updateContactById(id, data) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { ...contacts[index], ...data };
+  await updateContacts(contacts);
+
+  return contacts[index];
+}
